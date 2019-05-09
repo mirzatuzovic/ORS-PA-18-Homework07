b753c0f6fcef5fc7dce229d9f78a2755bdcb4aa8
Mirza Tuzovic <Mirza.Tuzovic@udg.edu.me>
2019-05-09 18:54:13 +0200

    Task3

--- task3.py
+++ task3.py
@@ -16,10 +16,28 @@
 # Write your function here
 
 
+from random import randint
 
-def main():
-    # Test your function here
-    pass
+def selection_sort(niz):
+    duzina = len(niz)
+
+    for pozicija in range(duzina - 1):
+
+        minimum = pozicija
+
+        for i in range(minimum + 1, duzina):
+            if niz[i] < niz[minimum]:
+                minimum = i
+
+        niz[pozicija], niz[minimum] = niz[minimum], niz[pozicija]
+
+    return niz
+
 
 if __name__ == "__main__":
-main()
+
+    niz = []
+    for i in range(1000):
+        niz.append(randint(0, 10000))
+    sortirani_niz = selection_sort(niz)
+    print(sortirani_niz)

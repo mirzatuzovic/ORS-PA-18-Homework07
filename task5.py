41a696b3f6849432ce9b55d227f320dfe29ba147
Mirza Tuzovic <Mirza.Tuzovic@udg.edu.me>
2019-05-09 22:49:53 +0200

    Task5

--- task5.py
+++ task5.py
@@ -25,11 +25,49 @@
 
 # Write your class here
 
+class Vehicle():
+
+    def __init__(self, company, model, year_of_production, registration_number, power, color):
+        self.company = company
+        self.model = model
+        self.year_of_production = year_of_production
+        self.registration_numberr = registration_number
+        self.power = power
+        self.color = color
+
+    def __str__(self):
+        return "Company that built that vehicle : {0}, Model of the vehicle: {1}, Year of product: {2}, Registration nuber: {3}, " \
+               "Engine power: {4}kW, Colour: {5}".format(self.company, self.model, self.year_of_production, self.registration_numberr, self.power, self.color)
+
+    def cost_of_registration(self):
+
+        if int(self.year_of_production)< 1990:
+            production_year_fee = 100
+        elif int(self.year_of_production)< 2000:
+            production_year_fee = 200
+        elif int(self.year_of_production)< 2010:
+            production_year_fee = 300
+        else:
+            production_year_fee = 400
+
+        fee1 = production_year_fee + int(self.power)*2
+        return fee1
+
+
+
+
+
+
+
+print("")
+vehicleNo1 = Vehicle("Audi", "A4", "2009", "PG-HD258", "77", "red")
+print(vehicleNo1)
+print("Registration for this vehicle will cost : " + str(Vehicle.cost_of_registration(vehicleNo1)) +" EUR")
 
 
 def main():
     # Test your function here
     pass
 
-if __name__ == "__main__":
+if _name_ == "_main_":
     main()

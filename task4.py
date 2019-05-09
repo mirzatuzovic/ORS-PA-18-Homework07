793243f04a03ee0edfe392245b45f960bb450c1f
Mirza Tuzovic <Mirza.Tuzovic@udg.edu.me>
2019-05-09 18:54:32 +0200

    Task4

--- task4.py
+++ task4.py
@@ -21,10 +21,53 @@
 # Write your function here
 
 
+def broj_pojavljivanja_u_listi(lista):
+
+    d= {}
+    duzina = len(lista)
+    broj_javljanja = 0
+
+    for i in range(duzina):
+
+        if lista[i] not in d:
+
+            for broj in range(i,duzina):
+
+
+                if lista[broj] == lista[i]:
+
+                    broj_javljanja += 1
+
+                dati_broj = lista[i]
+
+                d[dati_broj] = broj_javljanja
+        broj_javljanja=0
+
+    niz = []
+
+    for key in d:
+
+        niz.append(d[key])
+    maksimum = niz[0]
+
+    for i in range(len(niz)):
+        if niz[i]  >maksimum:
+            maksimum=niz[i]
+
+
+    for key in d:
+        if maksimum==d[key]:
+            return key
+
+
 
 def main():
-    # Test your function here
+
+    lista = [3,8,2,3,6,4,3,8,6,0,9,1,2,4,3]
+    x = broj_pojavljivanja_u_listi(lista)
+
+    print("U datoj listi se najvise pojavljuje broj: " + str (x))
     pass
 
 if __name__ == "__main__":
-main()
+    main()
